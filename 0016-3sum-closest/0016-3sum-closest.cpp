f86c172274089c91c23b9d272730fff3091a639c
ayusharyan4269-bit <ayusharyan4269@gmail.com>
2026-06-23 17:16:20 +0530

    Time: 8 ms (75.93%), Space: 13.9 MB (71.62%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -5,37 +5,49 @@ public:
         sort(nums.begin(), nums.end());
 
         int n = nums.size();
+        int sum = 0;
+        int diff;
+        int min_diff = INT_MAX;
 
-        int sum = nums[0] + nums[1] + nums[2];
-        int min_diff = abs(target - sum);
-
-        for(int i = 0; i < n - 2; i++) {
+        for(int i = 0; i < n; i++) {
 
             int left = i + 1;
-            int right = n - 1;
+            int right = n - 1;   
 
             while(left < right) {
 
-                int curr = nums[i] + nums[left] + nums[right];
-                int diff = abs(target - curr);
-
-                if(diff < min_diff) {
-                    min_diff = diff;
-                    sum = curr;
-                }
+                int curr = nums[left] + nums[right] + nums[i];
 
                 if(curr == target) {
-                    return target;
+                    return target;   
                 }
+
                 else if(curr < target) {
+
+                    diff = abs(target - curr);
+
+                    if(diff < min_diff) {
+                        min_diff = diff;
+                        sum = curr;
+                    }
+
                     left++;
                 }
+
                 else {
+
+                    diff = abs(curr - target);
+
+                    if(diff < min_diff) {
+                        min_diff = diff;
+                        sum = curr;
+                    }
+
                     right--;
                 }
             }
         }
 
-        return sum;
+        return sum;  
     }
 };
